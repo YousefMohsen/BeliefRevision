1e447343eb2160c816c3ba7ae3dd78b35063d090
YousefMohsen <yousefmohsen@outlook.dk>
2019-05-02 13:08:07 +0200

    project created

---
+++ src/Scripts/Main.js
@@ -0,0 +1,22 @@
+console.log('messi')
+
+
+let beliefBase ={}
+
+function addBelief(belief){//expansion
+//check if belief is in conflict with belief base
+//
+}
+
+function getEntailments(belief){
+    //return a list of logical entailments
+
+}
+
+
+
+//contraction
+//revision
+
+
+
