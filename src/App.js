35fe0d0eece2ac8239ed74786a622bc043328443
YousefMohsen <yousefmohsen@outlook.dk>
2019-05-02 14:32:43 +0200

    added ui

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      beliefBase: []
+    }
+
+  }
+
+  addBelief() {
+    let newBelief = this.state.beliefBase;
+    newBelief.push(this.state.input);
+    this.setState({ beliefBase: newBelief })
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <div className="left-panel">
+            <button onClick={this.addBelief.bind(this)}>Add belief </button>
+            <textarea onChange={(e) => this.setState({ input: e.target.value })} placeholder="Insert beliefs in propositional logic form" />
+          </div>
+          <div className="right-panel">
+            <h1>Belief base</h1>
+            {this.state.beliefBase.map((b) => b + "\n")}</div>
+
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
