f4049067920fe5d7a9e73d419842f65fe23167ff
YousefMohsen <yousefmohsen@outlook.dk>
2019-05-31 20:23:48 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import revisionHelper from './Scripts/Revision'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import engine from "./Scripts/RevisionEngine";
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      input: "¬PVQ\nP\n¬Q",
+      input: "¬QV(¬PVS)\nQ\nP",
       beliefBase: []
-    }
-
+    };
   }
 
   addBelief() {
     let beliefBase = this.state.beliefBase;
-    let newBeliefs = this.state.input
-    .trim()
-    .split("\n");
+    let newBeliefs = this.state.input.trim().split("\n");
     let newBelieefToAdd = newBeliefs[0];
-    if(newBelieefToAdd){
-    revisionHelper.updateBeliefBase(newBelieefToAdd,null, beliefBase)
-    console.log('newBlelif', newBeliefs )
-   newBeliefs.splice(0,1); //remove the added belief from texarea
-   // console.log('newBeliefs.join(n)',newBeliefs.join('\n'))
-    this.setState({input: newBeliefs.join('\n'),beliefBase })
-    /*
-    newBelief.push(this.state.input);
-    this.setState({ beliefBase: newBelief })*/
-
-  }
+    if (newBelieefToAdd) {
+      let newBB = engine.updateBeliefBase(newBelieefToAdd, null, beliefBase);
+      console.log("newBB", newBB);
+      newBeliefs.splice(0, 1); //remove the added belief from texarea
+      this.setState({ input: newBeliefs.join("\n"), beliefBase: newBB });
+    }
   }
   render() {
-console.log('this.state',this.state)
+    console.log("this.state", this.state);
     return (
       <div className="App">
         <div className="App-content">
           <div className="left-panel">
             <button onClick={this.addBelief.bind(this)}>Add belief </button>
-            <textarea value={this.state.input} onChange={(e) => this.setState({ input: e.target.value })} placeholder="Insert beliefs in propositional logic form" />
+            <textarea
+              value={this.state.input}
+              onChange={e => this.setState({ input: e.target.value })}
+              placeholder="Insert beliefs in propositional logic form"
+            />
           </div>
           <div className="right-panel">
             <h1>Belief base</h1>
-            {this.state.beliefBase.map((b) => b + "\n")}</div>
-
+            {this.state.beliefBase.map(b => b + "\n")}
+          </div>
         </div>
       </div>
     );
   }
-
 }
 
 export default App;
